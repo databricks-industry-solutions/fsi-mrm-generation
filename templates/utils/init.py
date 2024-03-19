a25213649fa3dae269e21ad3074d02795ca7c603
aamend <aamend>
2024-03-19 15:13:29 +0000

    Update solution for UC

--- templates/utils/init.py
+++ templates/utils/init.py
@@ -11,6 +11,12 @@ warnings.filterwarnings("ignore")
 
 # COMMAND ----------
 
+import mlflow
+# ensure models will be governed by UC
+mlflow.set_registry_uri('databricks-uc')
+
+# COMMAND ----------
+
 import seaborn as sns
 import matplotlib.pyplot as plt
 
@@ -31,29 +37,50 @@ from sklearn.pipeline import Pipeline
 from xgboost import XGBClassifier
 from xgboost import XGBRegressor
 
-import mlflow
 from mlflow.models import infer_signature
 
 # COMMAND ----------
 
-_ = sql("CREATE CATALOG IF NOT EXISTS fsgtm")
-_ = sql("DROP DATABASE IF EXISTS fsgtm.mrm CASCADE")
-_ = sql("CREATE DATABASE fsgtm.mrm")
+catalog = 'users'
+schema = dbutils.notebook.entry_point.getDbutils().notebook().getContext().userName().get()
+schema = schema.split('@')[0].replace('.', '_').lower()
+table_name = 'credit_data'
+volume_name = 'landing_zone'
 
 # COMMAND ----------
 
-df = pd.read_csv("data/german_credit_data_risk.csv", index_col=0)
-df = df.rename({
-  'Age' : 'AGE',
-  'Sex' : 'SEX',
-  'Job' : 'JOB',
-  'Housing': 'HOUSING',
-  'Saving accounts': 'SAVING_ACCOUNT',
-  'Checking account': 'CHECKING_ACCOUNT',
-  'Credit amount': 'CREDIT_AMOUNT',
-  'Duration': 'DURATION',
-  'Purpose': 'PURPOSE',
-  'Risk': 'RISK'
-}, axis=1)
+_ = sql(f"CREATE CATALOG IF NOT EXISTS {catalog}")
+_ = sql(f"CREATE DATABASE IF NOT EXISTS {catalog}.{schema}")
+_ = sql(f"CREATE VOLUME IF NOT EXISTS {catalog}.{schema}.{volume_name}")
 
-spark.createDataFrame(df).write.format('delta').saveAsTable('fsgtm.mrm.german_credit_data_risk')
+# COMMAND ----------
+
+import shutil
+shutil.copy('data/german_credit_data_risk.csv', f'/Volumes/{catalog}/{schema}/{volume_name}/german_credit_data_risk.csv')
+
+# COMMAND ----------
+
+from pyspark.sql import functions as F
+
+volume_df = (
+  spark
+    .read
+    .format('csv')
+    .option('header', 'true')
+    .option('inferSchema', 'true')
+    .load(f'/Volumes/{catalog}/{schema}/{volume_name}')
+    .select(
+      F.col('Age').alias('AGE'),
+      F.col('Sex').alias('SEX'),
+      F.col('Job').alias('JOB'),
+      F.col('Housing').alias('HOUSING'),
+      F.col('Saving accounts').alias('SAVING_ACCOUNT'),
+      F.col('Checking account').alias('CHECKING_ACCOUNT'),
+      F.col('Credit amount').alias('CREDIT_AMOUNT'),
+      F.col('Duration').alias('DURATION'),
+      F.col('Purpose').alias('PURPOSE'),
+      F.col('Risk').alias('RISK')
+    )
+)
+
+volume_df.write.mode('overwrite').format('delta').saveAsTable(f'{catalog}.{schema}.{table_name}')
